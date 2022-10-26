3d5a71b23bfb0072b1c84725c274f301655b0dad
deoj <deoj.phil@yahoo.ca>
2022-10-26 13:34:57 -0600

    added files

---
+++ js/client.js
@@ -0,0 +1,15 @@
+// Get the sum of two arrays…actually the sum of all their elements.
+// P.S. Each array includes only integer numbers. Output is a number too.
+
+let arr_1 = [3, 5, 22, 5,  7,  2,  45, 75, 89, 21, 2]; // --> 276
+let arr_2 = [9, 2, 42, 55, 71, 22, 4,  5,  90, 25, 26]; // --> 351
+// Example output: 
+// 276 + 351 = 627
+
+
+let result = 0;
+for (let i = 0; i < arr_1.length; i++) {
+  result += (arr_1[i] + arr_2[i]);
+}
+
+console.log(result);
